33894a12400aa83ef375e2ebbd32a132b4026171
Thomas Guymer <t.m.guymer@thomasguymer.co.uk>
2022-07-02 12:47:10 +0100

    replace os.path.join with f-strings

--- funcs/__init__.py
+++ funcs/__init__.py
@@ -1,6 +1,3 @@
-# Import standard modules ...
-import sys
-
 # Import sub-functions ...
 from .calcHorizontalGridlines import calcHorizontalGridlines
 from .calcVerticalGridlines import calcVerticalGridlines
@@ -9,7 +6,3 @@ from .findFractionOfPixelWithinCircle import findFractionOfPixelWithinCircle
 from .rasterizePolygon import rasterizePolygon
 from .rasterizeShapefile import rasterizeShapefile
 from .sumImageWithinCircle import sumImageWithinCircle
-
-# Ensure that this module is only imported by Python 3.x ...
-if sys.version_info.major != 3:
-    raise Exception("this Python module must only be used with Python 3.x") from None
